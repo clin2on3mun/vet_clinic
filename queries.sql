1a0cc4a57d78ed564783b3735f55bf4e74ebbf33
clin2on3mun <munanaclinton@gmail.com>
2023-08-14 12:23:18 +0200

    Add new query

--- queries.sql
+++ queries.sql
@@ -3,6 +3,7 @@
 SELECT name FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth >= '2016-01-01' AND date_of_birth <= '2019-12-31';
 SELECT name FROM animals WHERE neutered=true AND escape_attempts < 3;
+SELECT name, escape_attempts from animals WHERE weight_kg >10.5;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name='Pikachu';
 SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
