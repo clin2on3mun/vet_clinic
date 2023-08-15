3c666c840234e1d06a99d23d02fa58cd018d5fd8
clin2on3mun <munanaclinton@gmail.com>
2023-08-15 11:45:32 +0200

    Add more data to animal table

--- data.sql
+++ data.sql
@@ -2,3 +2,6 @@
 
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Agumon', 'feb-3-2020', 0, true, 10.23),
 ('Gabumon', 'Nov-15-2018', 2, true, 8),('Pikachu', 'Jan-7-2021', 1, false, 15.04),('Devimon', 'May-12-2017', 5, true, 11);
+INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Charmander', 'FEB-8-2020', 0,false, -11),
+('Plantmon', 'NOV-15-2021',2,true,-5.7),('Squirtle', 'APR-2-1993', 3, false,-12.13),('Angemon', 'jun-12-2005',1,true, -45),
+('Boarmon', 'jun-7-2005', 7, true, 20.4),('Blossom', 'OCT-13-1998', 3, true, 17),('Ditto', 'May-14-2022', 4, true, 22);
