5950eadfa0c002e9f0ae01b93df4a868ac42ef39
clin2on3mun <munanaclinton@gmail.com>
2023-08-16 11:01:20 +0200

    Add new tables owners and species

--- schema.sql
+++ schema.sql
@@ -11,3 +11,22 @@ CREATE TABLE animals (
     PRIMARY KEY(id)
 );
 ALTER TABLE animals ADD COLUMN species VARCHAR(30);
+
+CREATE TABLE owners(
+    ID INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(100),
+    age INT,
+    PRIMARY KEY(ID)
+);
+
+CREATE TABLE species(
+    ID INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(30),
+    PRIMARY KEY(ID)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INTEGER,
+ADD CONSTRAINT fk_species_id FOREIGN KEY(species_id) REFERENCES species(ID);
+ALTER TABLE animals ADD COLUMN owner_id INTEGER,
+ADD CONSTRAINT fk_owner_id FOREIGN KEY(owner_id) REFERENCES owners(ID);
