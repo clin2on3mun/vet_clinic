5778322ac08643c5d4e144d26b97a755e37f716e
clin2on3mun <munanaclinton@gmail.com>
2023-08-17 12:34:06 +0200

    Add new table visits, specialization and vets

--- schema.sql
+++ schema.sql
@@ -30,3 +30,30 @@ ALTER TABLE animals ADD COLUMN species_id INTEGER,
 ADD CONSTRAINT fk_species_id FOREIGN KEY(species_id) REFERENCES species(ID);
 ALTER TABLE animals ADD COLUMN owner_id INTEGER,
 ADD CONSTRAINT fk_owner_id FOREIGN KEY(owner_id) REFERENCES owners(ID);
+
+CREATE TABLE vets (
+    ID INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(ID)
+);
+
+CREATE TABLE specializations(
+   ID INT GENERATED ALWAYS AS IDENTITY,
+   vet INT,
+   species INT,
+   CONSTRAINT fk_vet FOREIGN KEY(vet) REFERENCES vets(ID),
+   CONSTRAINT fk_species FOREIGN KEY(species) REFERENCES species(ID),
+   PRIMARY KEY(ID)
+)
+
+CREATE TABLE visits (
+    ID INT GENERATED ALWAYS AS IDENTITY,
+    animal_id INT,
+    vet_id INT,
+    date_of_visit DATE,
+    CONSTRAINT fk_animal FOREIGN KEY(animal_id) REFERENCES animals(ID),
+    CONSTRAINT fk_vet FOREIGN KEY(vet_id) REFERENCES vets(ID),
+    PRIMARY KEY(ID)
+)
