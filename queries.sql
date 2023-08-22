41db68772b1f9d7b7fb68016b490d5e1a137450d
clin2on3mun <munanaclinton@gmail.com>
2023-08-22 11:27:29 +0200

    Add new queries to track database performance

--- queries.sql
+++ queries.sql
@@ -139,4 +139,8 @@ WHERE vet.name = 'Maisy Smith'
 GROUP BY species.name
 LIMIT 1;
 
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
 
+explain analyze SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
